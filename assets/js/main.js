fd1df025c0eacfa7fe7ccd412ac125b5d8294397
Ryan McGovern <ryanmcgovern@Xpand-Dev-Mac.lan>
2017-02-28 11:06:47 +0000

    Add comments to the main.js to explain the function for using Ajax on categories

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 (function($){
-	// load content from category pages using AJAX 
+	/* Load content from category pages using AJAX 
+	 * NOTE: to make this work you will need to make sure
+	 * the content area you are trying to load includes 
+	 * elements with the ids #main and #inner
+	 */ 
+	
 	var $mainContent = $('#main'),
 			$cat_links = $('ul.categories-filters li a');
 			
