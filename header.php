66bce2938fd95a4d4121970c0181c7299534a411
Ryan McGovern <ryanmcgovern@Xpand-Dev-Mac.lan>
2017-08-25 16:39:46 +0100

    Added IE8 warning box to header.php

--- header.php
+++ header.php
@@ -4,7 +4,14 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
+	<!--[if lt IE 9]>
+	<div class="old-browser-container">
+		<div class="old-browser-message">
+			<div class="o-b-title">Your browser is outdated</div>
+				<div class="o-b-message">Your browser is out of date, and some things may not look right. For an optimal viewing expierence, please <a href="https://www.google.com/chrome/index.html" target="_blank">update your browser.</a>
+			</div>
+		</div>
+	</div><![endif]--> 
 	<?php wp_head(); ?>
 </head>
 
